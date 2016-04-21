8480c9f24d6a84ffb46de677a13734ec159f5249
loveKino <842914439@qq.com>
2016-04-21 23:59:47 +0800

    stage-local

--- src/meta.js
+++ src/meta.js
@@ -3,8 +3,15 @@
 /**
  * meta expression
  *
- * A._(0) -> { index, array, type }
- * f._(0) -> { args, fun }
+ * mock some expression and translate to ast
+ *
+ * array get element expression
+ *
+ * A[0] => A._(0) -> { index, array, type }
+ *
+ * function run expression
+ *
+ * f(0) => f._(0) -> { args, fun }
  */
 
 module.exports = () => {
